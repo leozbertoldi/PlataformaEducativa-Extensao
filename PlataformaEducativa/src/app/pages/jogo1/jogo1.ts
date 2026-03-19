52a5ce561db82e9fb20b5252f82dc3128f3b1c80
leozbertoldi <leozbertoldi@gmail.com>
2026-03-19 10:51:31 -0300

    [ADD] Estrtuturação inicial do projeto

---
+++ PlataformaEducativa/src/app/pages/jogo1/jogo1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jogo1',
+  imports: [],
+  templateUrl: './jogo1.html',
+  styleUrl: './jogo1.css',
+})
+export class Jogo1 {
+
+}
