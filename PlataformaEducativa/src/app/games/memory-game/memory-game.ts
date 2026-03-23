296455d759e8a5746dd9bb959bc78025180f66c8
leozbertoldi <leozbertoldi@gmail.com>
2026-03-23 09:17:45 -0300

    [ADD] Finalizado jogo da memória

--- PlataformaEducativa/src/app/games/memory-game/memory-game.ts
+++ PlataformaEducativa/src/app/games/memory-game/memory-game.ts
@@ -13,6 +13,12 @@ export class MemoryGame {
   round = signal(0);
   sequence: number[] = [];
   userSequence: number[] = [];
+  activeCard = signal<number | null>(null);
+  feedbackCard = signal<number | null>(null);
+  feedbackType = signal<'correct' | 'wrong' | null>(null);
+  gameOver = signal(false); 
+  clickedCard = signal<number | null>(null);
+  isCorrectClick = signal<boolean | null>(null);
 
   ngOnInit() {
     this.startGame();
@@ -23,6 +29,7 @@ export class MemoryGame {
     this.round.set(0);
     this.sequence = [];
     this.userSequence = [];
+    this.gameOver.set(false);
     this.nextRound();
   }
 
@@ -39,14 +46,28 @@ export class MemoryGame {
     console.log('Playing sequence:', this.sequence);
     this.isPlaying.set(true);
     let index = 0;
+    
     const interval = setInterval(() => {
-      if (index < this.sequence.length) {
+      if (index < this.sequence.length) 
+      {
+
         console.log('Playing card:', this.sequence[index]);
+        const card = this.sequence[index];
+        this.activeCard.set(card); // ativa animação
+        this.sequence[index];
+
+        setTimeout(() => {
+          this.activeCard.set(null); // desativa depois de um tempo
+        }, 500);
+
         index++;
-      } else {
+      } 
+      else 
+      {
         clearInterval(interval);
         this.isPlaying.set(false);
       }
+
     }, 1000);
   }
 
@@ -57,10 +78,24 @@ export class MemoryGame {
     this.userSequence.push(card);
     const currentIndex = this.userSequence.length - 1;
 
+    const correct = this.sequence[currentIndex] === card;
+
+    this.clickedCard.set(card);
+    this.isCorrectClick.set(correct);
+
+    setTimeout(() => {
+      this.clickedCard.set(null);
+      this.isCorrectClick.set(null);
+    }, 200);
+
     if (this.userSequence[currentIndex] !== this.sequence[currentIndex]) {
-      this.isPlaying.set(false);
-      alert(`Game Over! You reached round ${this.round()}.`);
-      this.startGame();
+      this.gameOver.set(true);
+      setTimeout(() => {
+        this.gameOver.set(false); // desativa depois de um tempo
+      
+        this.isPlaying.set(false);
+        this.startGame();
+      }, 2000);
     } else if (this.userSequence.length === this.sequence.length) {
       setTimeout(() => this.nextRound(), 1000);
     }
